22a24d86d54c185c226f7b4e1b086cd3a2eca962
Ashirwad Sangwan <ashi22744@gmail.com>
2019-05-18 17:39:05 +0530

    changes in video-analysis.py

--- sentdex-tutorial/Video-Analysis/video_analysis.py
+++ sentdex-tutorial/Video-Analysis/video_analysis.py
@@ -11,7 +11,11 @@ while loop to get the VideoCapture working.
 
 while True:
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
     cv2.imshow('frame', frame)
+    cv2.imshow('gray', gray)
+
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
